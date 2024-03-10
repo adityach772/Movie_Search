e5789cddc84493e239d0bbcb1b35ad31f7ec37fa
aditya-772 <adityachundims@gmail.com>
2024-03-09 20:32:12 -0800

    Movie_Search_V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [counter,setCounter] = useState(0);
+  //useEffect(() => {alert("you have changed counter to" + counter)}, [counter]);
+  const decrement = () => {setCounter(counter-1)}
+  const increment = () => {setCounter(counter+1)}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={decrement}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
