47ee5428bf4c29c35da37879690fe474dd6b923e
aditya-772 <adityachundims@gmail.com>
2024-03-10 00:35:19 -0800

    Default Value Change

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
     }
    }
    useEffect( () => {
-    searchMovies("Disney");
+    searchMovies("Mission Impossible");
    },[]); //only want to call after start 
 
     return (
